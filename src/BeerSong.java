e9dd70434bbc23ee3564fe182f7a6d24da40b4e1
Brandon Jackson <bwjackson@oakland.edu>
2016-02-04 21:05:03 -0500

    Project initialized. Main and 99 Beers function skeletons added

---
+++ src/BeerSong.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Brandon on 2/4/2016.
+ */
+public class BeerSong {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void Ninety_Nine_Bottles_of_Beer() {
+        
+    }
+}
