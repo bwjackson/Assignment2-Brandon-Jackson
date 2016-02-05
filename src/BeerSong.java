b173b0dc162add5e7227c52f9c6d0eb38bb3f409
Brandon Jackson <bwjackson@oakland.edu>
2016-02-04 21:09:17 -0500

    Implement main method

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -4,10 +4,10 @@
 public class BeerSong {
 
     public static void main(String[] args) {
-
+        Ninety_Nine_Bottles_of_Beer();
     }
 
     public static void Ninety_Nine_Bottles_of_Beer() {
-        
+        System.out.println("Beer Song");
     }
 }
